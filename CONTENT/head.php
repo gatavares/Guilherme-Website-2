18af32b4852cec2494256d74de9aaf28704c365b
Guilherme Tavares <joseserafimtavares89@gmail.com>
2022-06-24 18:29:11 +0100

    Start Project

---
+++ CONTENT/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guilherme Tavares</title>
+    <link rel="icon" type="image/x-icon" href="ASSETS/favicon.png">
+
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="CSS/styles.css">
+    <!-- FONTAWSOME ICONS -->
+    <link href="CSS/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+</head>
+<body>
