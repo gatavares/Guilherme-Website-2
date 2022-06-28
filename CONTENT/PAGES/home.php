bbdf86ec66bf0ef6fba635b4ce6cf82faf590fc9
Guilherme Tavares <joseserafimtavares89@gmail.com>
2022-06-28 22:04:56 +0100

    Inicio de projeto, home e navs (#1)

---
+++ CONTENT/PAGES/home.php
@@ -0,0 +1,52 @@
+<div class="home">
+    <section class="homeContent">
+        <div class="homeTitle text-center">
+            <h2>QUEM SOU EU?</h2>
+        </div>
+        <div class="homeText">
+            <div class="container">
+                <div class="row g-5">
+                    <div class="col-12 col-md-7">
+                        <div class="p-3 bg-ghost">
+                            <p>
+                                Olá, eu sou o Guilherme Tavares!<br>
+                                Sou um programador júnior e estou-me a formar na <span>Escola Profissional Ruiz Costa</span>.
+                            </p>
+                            <p>
+                                Comecei a programar com 14 anos, em casa, alguns joguinhos e adorei, logo, interessei-me
+                                mais e decidi aprofundar no ramo da programação. Descobri o ramo de FrontEnd de sites, e
+                                decidi fazer o meu primeiro site através de cursos gratuitos no W3 e no YouTube.
+                            </p>
+                            <p>
+                                Comecei a fazer sites para aprender e em 2020 não sabia ao certo o que fazer da minha
+                                vida. Neste ano, descobri através de um amigo que uma escola que estava a formar novos
+                                programadores e a prepará-los para o mercado e nem pensei duas vezes, candidatei-me a
+                                uma vaga numa turma de Programação na <span>Escola Profissional Ruiz Costa</span>.
+                            </p>
+                            <p>
+                                Comecei a fazer sites para aprender e em 2020 não sabia ao certo o que fazer da minha
+                                vida. Neste ano, descobri através de um amigo que uma escola que estava a formar novos
+                                programadores e a prepará-los para o mercado e nem pensei duas vezes, candidatei-me a
+                                uma vaga numa turma de Programação na <span>Escola Profissional Ruiz Costa</span>.
+                            </p>
+                            <p>
+                                Hoje ainda frequento esta escola, no curso de <span>Gestão e Programação de Sistemas
+                                Informáticos</span> e tenho a certeza que se tivesse outra chance de escolher, não mudaria
+                                nada!
+                            </p>
+                            <p>
+                                Com 17 anos, já trabalhei para 3 empresas e estou a estagiar numa empresa de programação
+                                "web in code" através da minha escola.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5 text-center">
+                        <div class="p-3 Foto">
+                            <img src="ASSETS/Screenshot_1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- CONTENT/head.php
+++ CONTENT/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-PT">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,11 +8,26 @@
     <title>Guilherme Tavares</title>
     <link rel="icon" type="image/x-icon" href="ASSETS/favicon.png">
 
+
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;600&display=swap" rel="stylesheet">
+
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="CSS/bootstrap.min.css">
+    <!-- FONTAWSOME ICONS -->
+    <link href="ASSETS/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
     <!-- CSS -->
     <link rel="stylesheet" href="CSS/styles.css">
-    <!-- FONTAWSOME ICONS -->
-    <link href="CSS/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
 </head>
+
 <body>
