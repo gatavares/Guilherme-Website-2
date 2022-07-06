b2b9b1901a031090dbe4eb3698360b5993d9c1e6
Guilherme Tavares <joseserafimtavares89@gmail.com>
2022-07-06 17:22:14 +0100

    Atualizacao 6-05

--- CONTENT/PAGES/programacao.php
+++ CONTENT/PAGES/programacao.php
@@ -79,52 +79,35 @@
                     <div class="col-12 col-md-7">
                         <div class="p-3 bg-ghost">
                             <p>
-                                O <span>Professional World Podcast</span> foi um projeto que eu fundei com mais 5
-                                colegas de turma,
-                                com fim de dar a conhecer mais os cursos profissionais da minha escola e contar
-                                histórias de professores e ex-alunos dos diversos cursos. Podes conferir todo o projeto
-                                <a href="https://www.youtube.com/channel/UCLYF0O1ROgP1sYi-8ZBJpsg"
-                                    target="_blank"><span>aqui</span></a>.
+                                A <span>Fisio Respira</span> é um projeto revolucionário no mundo, desenvolvido por mim
+                                e pela
+                                <a href="" target="" rel="noopener noreferrer"><br><span>Mariana Gabriel</span></a>.
+                                É a primeira aplicação/pagina de fisioterapia respiratória, com acompanhamento
+                                personalizado e com avaliações periódicas de todos os utentes.
                             </p>
                             <p>
-                                Apesar do êxito e crescimento, este projeto foi interrompido para podermo-nos focar para
-                                as provas que iríamos ter e também para podermos reorganizar, acabando assim por não
-                                retornar.
+                                Este projeto consiste numa aplicação que estará disponível, brevemente, na APPLE Store
+                                (Dispositivos Apple) e na Play Store (Dispositivos Android), e que serve para informar
+                                as pessoas de causas e tratamentos para problemas que necessitem de fisioterapia
+                                respiratória.
                             </p>
                             <p>
-                                <br>
-                                Infelizmente este site nunca foi inserido no seu domínio, devido ao projeto ter sido
-                                interrompido!
+                                Estes clientes que acharem que se encontram em algum caso de ter problemas respiratórios
+                                e/ou tenha sido aconselhado o tratamento por via da fisioterapia. Esta aplicação também
+                                serve para atletas que necessitem treinar e/ou melhorar a sua respiração.
+                            </p>
+                            <p>
+                                Queres saber mais sobre este projeto? Clica <a
+                                    href="https://www.youtube.com/channel/UCLYF0O1ROgP1sYi-8ZBJpsg"
+                                    target="_blank"><span>aqui</span></a>!
                             </p>
                         </div>
                     </div>
                     <div class="col-12 col-md-5 text-center">
                         <div class="p-3 Foto">
-                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-indicators">
-                                    <button type="button" data-bs-target="#carouselExampleIndicators"
-                                        data-bs-slide-to="0" class="active" aria-current="true"
-                                        aria-label="Slide 1"></button>
-                                    <button type="button" data-bs-target="#carouselExampleIndicators"
-                                        data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                    <button type="button" data-bs-target="#carouselExampleIndicators"
-                                        data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                </div>
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active" data-bs-interval="5000">
-                                        <img src="ASSETS/site-podcast.png" class="d-block w-100"
-                                            alt="Foto Site Podcast1">
-                                    </div>
-                                    <div class="carousel-item" data-bs-interval="5000">
-                                        <img src="ASSETS/site-podcast2.png" class="d-block w-100"
-                                            alt="Foto Site Podcast2">
-                                    </div>
-                                    <div class="carousel-item" data-bs-interval="5000">
-                                        <img src="ASSETS/site-podcast3.png" class="d-block w-100"
-                                            alt="Foto Site Podcast3">
-                                    </div>
-                                </div>
-                            </div>
+                            <video
+                                src="https://firebasestorage.googleapis.com/v0/b/fisio-appgpsi.appspot.com/o/VideosApresentacao%2FFisioRespira2.mp4?alt=media&token=f6d95d76-f50c-4fb8-b0c1-47dd45976053"
+                                width="500" height="281.25" loop="true" autoplay="autoplay" muted></video>
                         </div>
                     </div>
                 </div>
